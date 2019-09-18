7cc5e3093a96f664bb22d6e6fb7beaf80e09b469
wb135 <wb135@yingu.com>
2019-09-18 20:43:02 +0800

    1.init

--- src/router.js
+++ src/router.js
@@ -1,6 +1,13 @@
+/*
+ * @Description: 配置路由
+ * @Author: your name
+ * @Date: 2019-09-18 20:06:10
+ * @LastEditTime: 2019-09-18 20:40:49
+ * @LastEditors: Please set LastEditors
+ */
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from './views/Home'
 
 Vue.use(Router)
 
@@ -10,14 +17,14 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
+    // {
+    // path: '/about',
+    // name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    // component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // }
   ]
 })
---
+++ src/views/Home/index.vue
@@ -0,0 +1,22 @@
+<!--
+ @Description: 首页
+ * @Author: your name
+ * @Date: 2019-09-18 20:11:39
+ * @LastEditTime: 2019-09-18 20:25:52
+ * @LastEditors: Please set LastEditors
+ -->
+<template>
+  <div>
+      this is indexPage
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
