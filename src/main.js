967845a361be72815715d4e2057f26524367ee02
wb135 <wb135@yingu.com>
2019-09-18 21:06:53 +0800

    3.重置全局样式,添加index.css文件

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-09-18 20:06:10
- * @LastEditTime: 2019-09-18 20:48:25
+ * @LastEditTime: 2019-09-18 21:05:35
  * @LastEditors: Please set LastEditors
  */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 重置全局样式
+import './styles/index.less'
 // 注册elementUi
 import ElementUi from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
