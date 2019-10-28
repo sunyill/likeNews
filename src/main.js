22f6902234ac50f5043e449a2fb214e94f4a926c
sunyill <sunyill0707@163.com>
2019-10-28 15:50:53 +0800

    6.配置axios

--- src/main.js
+++ src/main.js
@@ -13,7 +13,11 @@ import './styles/index.less'
 // 注册elementUi
 import ElementUi from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-
+import axios from 'axios'
+// 配置axios的默认url
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+// 将axios的插件给Vue的原型
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 // 注册全局组件
 Vue.use(ElementUi)
