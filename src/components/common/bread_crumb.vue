f708db71b617748ec46d4f42cc14130d557049fb
sunyill <sunyill0707@163.com>
2019-11-05 16:33:11 +0800

    22.全局|封装面包屑(bread-crumb)组件

---
+++ src/components/common/bread_crumb.vue
@@ -0,0 +1,21 @@
+<template>
+<!-- 面包屑封装的意义 -->
+<!-- 显示当前页面的路径,可以直观的查看页面 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item :to="{path:'/'}"></el-breadcrumb-item>
+        <el-breadcrumb-item>
+            <!-- 定义具名插槽 -->
+            <slot name="title"></slot>
+        </el-breadcrumb-item>
+    </el-breadcrumb>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@
  */
 import layoutAside from './home/layout_aside.vue'
 import layoutHeader from './home/layout_header.vue'
+import breadCrumb from './common/bread_crumb.vue'
 
 // elementUI中导出的一个对象,并提供一个方法 install
 export default {
   install (Vue) {
     Vue.component('layout-aside', layoutAside)
     Vue.component('layout-header', layoutHeader)
+    Vue.component('bread-crumb', breadCrumb) // 注册面包屑组件
   }
 }
