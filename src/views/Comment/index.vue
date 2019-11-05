fb99037923753a9764ea75d30a14d3ffa048f210
sunyill <sunyill0707@163.com>
2019-11-05 18:32:36 +0800

    26.评论列表|修改或者关闭评论

--- src/views/Comment/index.vue
+++ src/views/Comment/index.vue
@@ -1,22 +1,29 @@
 <template>
-    <el-card>
-        <!-- 面包屑 -->
-        <bread-crumb slot="header">
-            <template slot="title">评论列表</template>
-        </bread-crumb>
+  <el-card>
+    <!-- 面包屑 -->
+    <bread-crumb slot="header">
+      <template slot="title">评论列表</template>
+    </bread-crumb>
 
-        <!-- 使用table展示页面 -->
-        <el-table :data="list" stripe>
-            <el-table-column width="500" label="标题" prop="title"></el-table-column>
-            <el-table-column label="评论状态" :formatter="formatter" prop="comment_status"></el-table-column>
-            <el-table-column label="总评论数" prop="total_comment_count"></el-table-column>
-            <el-table-column label="粉丝评论数" prop="fans_comment_count"></el-table-column>
-            <el-table-column label="操作" prop="fans_operate">
-                <el-button type="text">修改</el-button>
-                <el-button type="text">关闭评论</el-button>
-            </el-table-column>
-        </el-table>
-    </el-card>
+    <!-- 使用table展示页面 -->
+    <el-table :data="list" stripe>
+      <el-table-column width="500" label="标题" prop="title"></el-table-column>
+      <el-table-column label="评论状态" :formatter="formatter" prop="comment_status"></el-table-column>
+      <el-table-column label="总评论数" prop="total_comment_count"></el-table-column>
+      <el-table-column label="粉丝评论数" prop="fans_comment_count"></el-table-column>
+      <el-table-column label="操作" prop="fans_operate">
+        <!-- 作用域插槽 -->
+        <template slot-scope="obj">
+          <el-button type="text">修改评论</el-button>
+          <el-button
+            type="text"
+            @click="closeOrOpen(obj.row)"
+            :style="{color:obj.row.comment_status?'#E6A23C' : '#409EFF'}"
+          >{{obj.row.comment_status?'关闭评论':"打开评论"}}</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script>
@@ -27,6 +34,24 @@ export default {
     }
   },
   methods: {
+    closeOrOpen (row) {
+      let msg = row.comment_status ? '关闭' : '打开'
+      this.$confirm(`您确定要${msg}评论吗?`, '提示').then(() => {
+        this.$axios({
+          url: '/comments/status',
+          method: 'put',
+          params: {
+            article_id: row.id
+          },
+          data: {
+            allow_comment: !row.comment_status
+          }
+        }).then(() => {
+          // 如果走到then, 说明上面已通过
+          this.getComment()
+        })
+      })
+    },
     getComment () {
       this.$axios({
         url: '/articles',
@@ -56,5 +81,4 @@ export default {
 </script>
 
 <style>
-
 </style>
