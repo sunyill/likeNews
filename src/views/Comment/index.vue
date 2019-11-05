8c2c02df1f9b719f68f4f4162d3be1ca4bb4dfb5
sunyill <sunyill0707@163.com>
2019-11-05 17:16:17 +0800

    24.侧边栏|评论列表,使用table搭建列表

--- src/views/Comment/index.vue
+++ src/views/Comment/index.vue
@@ -1,12 +1,28 @@
 <template>
-    <div>
-        评论列表页面
-    </div>
+    <el-card>
+        <!-- 面包屑 -->
+        <bread-crumb slot="header">
+            <template slot="title">评论列表</template>
+        </bread-crumb>
+
+        <!-- 使用table展示页面 -->
+        <el-table>
+            <el-table-column width="500" label="标题"></el-table-column>
+            <el-table-column label="评论状态"></el-table-column>
+            <el-table-column label="总评论数"></el-table-column>
+            <el-table-column label="粉丝评论数"></el-table-column>
+            <el-table-column label="操作"></el-table-column>
+        </el-table>
+    </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      list: []
+    }
+  }
 }
 </script>
 
