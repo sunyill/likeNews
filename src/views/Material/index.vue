96ddff9520bae22449198285eb72fe230f3cad08
sunyill <sunyill0707@163.com>
2019-11-07 13:46:50 +0800

    31.素材列表|界面搭建

--- src/views/Material/index.vue
+++ src/views/Material/index.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>素材列表</div>
+  <el-card>
+    <bread-crumb slot="header">
+      <template slot="title">素材管理</template>
+    </bread-crumb>
+    <!--Tabs 标签页 分隔内容上有关联但属于不同类别的数据集合。 -->
+    <el-tabs v-model="activeName" >
+      <el-tab-pane label="全部图片" name="first">全部图片</el-tab-pane>
+      <el-tab-pane label="收藏图片" name="second">收藏图片</el-tab-pane>
+    </el-tabs>
+  </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      activeName: 'first'
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
