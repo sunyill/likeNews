4ed99643f5e8e3dd986d269919ab558d0ab89a12
sunyill <sunyill0707@163.com>
2019-11-11 13:41:16 +0800

    43.发布文章|发布文章逻辑

--- src/views/Publish/index.vue
+++ src/views/Publish/index.vue
@@ -3,29 +3,29 @@
     <bread-crumb slot="header">
       <template slot="title">文章发布</template>
     </bread-crumb>
-    <el-form label-width="80px" class="publish-form">
-      <el-form-item label="标题">
-        <el-input style="width:400px"></el-input>
+    <el-form ref="myForm" :rules="rules" :model="formData" label-width="80px" class="publish-form">
+      <el-form-item label="标题" prop="title">
+        <el-input style="width:400px" v-model="formData.title"></el-input>
       </el-form-item>
-      <el-form-item label="内容">
-        <el-input placeholder="请输入内容" type="textarea" :rows="4" v-model="textarea"></el-input>
+      <el-form-item label="内容" prop="content">
+        <el-input placeholder="请输入内容" v-model="formData.content" type="textarea" :rows="4"></el-input>
       </el-form-item>
       <el-form-item label="封面">
-        <el-radio-group>
-          <el-radio>单图</el-radio>
-          <el-radio>三图</el-radio>
-          <el-radio>无图</el-radio>
-          <el-radio>自动</el-radio>
+        <el-radio-group prop="formData.cover">
+          <el-radio :label="1">单图</el-radio>
+          <el-radio :label="3">三图</el-radio>
+          <el-radio :label="0">无图</el-radio>
+          <el-radio :label="-1">自动</el-radio>
         </el-radio-group>
       </el-form-item>
-      <el-form-item label="频道">
-        <el-select>
+      <el-form-item label="频道" prop="channel_id">
+        <el-select v-model="formData.channel_id">
           <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
         </el-select>
       </el-form-item>
 
       <el-form-item>
-        <el-button type="primary">发布</el-button>
+        <el-button type="primary" @click="publish">发布</el-button>
         <el-button>存入草稿</el-button>
       </el-form-item>
     </el-form>
@@ -36,16 +36,36 @@
 export default {
   data () {
     return {
-      channels: []
+      channels: [],
+      formData: {
+        title: '',
+        content: '',
+        cover: 0, // 默认无图
+        channel_id: null
+      },
+      rules: {
+        title: [{ required: true, message: '标题不能为空' }],
+        content: [{ required: true, message: '内容不能为空' }],
+        channel_id: [{ required: true, message: '频道不能为空' }]
+      }
     }
   },
   methods: {
+    // 频道列表
     getChannels () {
       this.$axios({
         url: '/channels'
-      }).then((res) => {
-        console.log(res.data.channels)
+      }).then(res => {
+        // console.log(res.data.channels)
         this.channels = res.data.channels
+      })
+    },
+    // 发布文章
+    publish () {
+      this.$refs.myForm.validate(isOK => {
+        if (isOK) {
+          console.log('ok')
+        }
       })
     }
   },
