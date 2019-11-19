a53f32e058d958300d2a1a2d118df8a42f93866b
sunyill <sunyill0707@163.com>
2019-11-19 17:25:39 +0800

    54.用户信息|请求网络数据展示

--- src/views/Account/index.vue
+++ src/views/Account/index.vue
@@ -6,21 +6,21 @@
     <!-- 表单 -->
     <el-form ref="myForm" label-width="100px">
       <el-form-item label="用户名称">
-        <el-input style="width:300px"></el-input>
+        <el-input v-model="formData.name" style="width:300px"></el-input>
       </el-form-item>
       <el-form-item label="用户简介">
-        <el-input style="width:300px"></el-input>
+        <el-input style="width:300px" v-model="formData.intro"></el-input>
       </el-form-item>
       <el-form-item label="用户邮箱">
-        <el-input style="width:300px"></el-input>
+        <el-input style="width:300px" v-model="formData.email"></el-input>
       </el-form-item>
       <el-form-item label="用户手机号">
-        <el-input style="width:300px" disabled></el-input>
+        <el-input style="width:300px" v-model="formData.mobile" disabled></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary">保存信息</el-button>
       </el-form-item>
-      <img class="head-img" src="../../assets/img/title_info.png" alt />
+      <img class="head-img" :src="formData.photo?formData.photo:imgUrl" alt />
     </el-form>
   </el-card>
 </template>
@@ -28,18 +28,39 @@
 <script>
 export default {
   data () {
-    return {}
+    return {
+      imgUrl: require('../../assets/img/title_info.png'),
+      formData: {
+        name: '',
+        intro: '',
+        email: '',
+        mobile: '',
+        photo: ''
+      }
+    }
+  },
+  methods: {
+    getPersonInfo () {
+      this.$axios({
+        url: '/user/profile'
+      }).then((res) => {
+        this.formData = res.data
+      })
+    }
+  },
+  created () {
+    this.getPersonInfo()
   }
 }
 </script>
 
 <style lang='less' scoped>
 .head-img {
-     position: absolute;
-     left: 700px;
-     top:150px;
-     height: 200px;
-     width: 200px;
-     border-radius: 50%;
- }
+  position: absolute;
+  left: 700px;
+  top: 150px;
+  height: 200px;
+  width: 200px;
+  border-radius: 50%;
+}
 </style>
