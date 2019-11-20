b30bee1d8dd34d21740310130253d4d63fc164b2
sunyill <sunyill0707@163.com>
2019-11-20 10:31:59 +0800

    55.用户详情|保存修改信息

--- src/views/Account/index.vue
+++ src/views/Account/index.vue
@@ -4,21 +4,21 @@
       <template slot="title">账号信息</template>
     </bread-crumb>
     <!-- 表单 -->
-    <el-form ref="myForm" label-width="100px">
-      <el-form-item label="用户名称">
+    <el-form ref="myForm" :model="formData" :rules="rules" label-width="100px">
+      <el-form-item label="用户名称" prop="name">
         <el-input v-model="formData.name" style="width:300px"></el-input>
       </el-form-item>
       <el-form-item label="用户简介">
         <el-input style="width:300px" v-model="formData.intro"></el-input>
       </el-form-item>
-      <el-form-item label="用户邮箱">
+      <el-form-item label="用户邮箱" prop="email">
         <el-input style="width:300px" v-model="formData.email"></el-input>
       </el-form-item>
       <el-form-item label="用户手机号">
         <el-input style="width:300px" v-model="formData.mobile" disabled></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary">保存信息</el-button>
+        <el-button type="primary" @click="saveUserInfo">保存信息</el-button>
       </el-form-item>
       <img class="head-img" :src="formData.photo?formData.photo:imgUrl" alt />
     </el-form>
@@ -36,6 +36,16 @@ export default {
         email: '',
         mobile: '',
         photo: ''
+      },
+      rules: {
+        name: [{ required: true, message: '用户名称不能为空' },
+          { min: 2, max: 10, message: '用户名称必须控制在2到10个字符' }
+        ],
+        email: [{
+          required: true, message: '邮箱不能为空'
+        },
+        { pattern: /^\w+((.\w+)|(-\w+))@[A-Za-z0-9]+((.|-)[A-Za-z0-9]+).[A-Za-z0-9]+$/, message: '邮箱格式不正确' }
+        ]
       }
     }
   },
@@ -45,6 +55,20 @@ export default {
         url: '/user/profile'
       }).then((res) => {
         this.formData = res.data
+      })
+    },
+    // 保存用户信息
+    saveUserInfo () {
+      this.$refs.myForm.validate((isOk) => {
+        if (isOk) {
+          this.$axios({
+            url: '/user/profile',
+            method: 'patch',
+            data: this.formData
+          }).then(() => {
+            this.$message({ message: '用户名保存成功', type: 'success' })
+          })
+        }
       })
     }
   },
--- src/views/Utils/axios.config.js
+++ src/views/Utils/axios.config.js
@@ -50,6 +50,9 @@ axios.interceptors.response.use(function (response) {
       window.localStorage.clear()
       window.location.hash('/login')
       break
+    case 409:
+      message = '用户名已存在'
+      break
     case 507:
       message = '服务器数据库异常'
       break
